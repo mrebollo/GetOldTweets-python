05a2ef851b2e7a6c303c9fbd6473728a505f7bc2
Migurel Rebollo <mrebollo@dsic.upv.es>
2019-07-31 13:43:33 +0200

    changes by execution of script

---
+++ getcomunica2.sh
@@ -0,0 +1,8 @@
+python2.7 Exporter.py --querysearch "#comunicat" --since 2011-02-26 --until 2011-02-29 --maxtweets 10000 --output comunica2011.csv
+python2.7 Exporter.py --querysearch "#comunica2" --since 2013-02-21 --until 2013-02-23 --maxtweets 10000 --output comunica2013.csv
+python2.7 Exporter.py --querysearch "#comunica2" --since 2014-02-20 --until 2014-02-22 --maxtweets 10000 --output comunica2014.csv
+python2.7 Exporter.py --querysearch "#comunica2" --since 2015-02-19 --until 2015-02-21 --maxtweets 10000 --output comunica2015.csv
+python2.7 Exporter.py --querysearch "#comunica2gandia" --since 2016-02-25 --until 2016-02-27 --maxtweets 10000 --output comunica2016.csv
+python2.7 Exporter.py --querysearch "#comunica2017" --since 2017-02-16 --until 2017-02-18 --maxtweets 10000 --output comunica2017.csv
+python2.7 Exporter.py --querysearch "#comunica2018" --since 2018-02-15 --until 2018-02-17 --maxtweets 10000 --output comunica2018.csv
+
